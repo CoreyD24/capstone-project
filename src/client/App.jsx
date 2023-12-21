8ac90d34582372c9e7cc3bfc08a3ebb3cd37e322
Jaylen <jmowens90@gmail.com>
2023-12-21 15:23:36 -0500

    added the SingleProduct component, added link to AllProducts component so when clicked it takes you to that product

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,14 +2,16 @@ import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import AllProducts from "./components/AllProducts";
+import SingleProduct from "./components/SingleProduct";
 
 const App = () => {
 
   return (
     <div className="App">
      <Routes>
-      <Route index path="/products" element={<AllProducts />}/>
-     </Routes>
+      <Route path="/products" element={<AllProducts />}/>
+      <Route path="/:id" element={<SingleProduct />}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/client/components/SingleProduct.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+const SingleProduct = () => {
+    const { id } = useParams();
+    const [ product, setProduct] = useState({});
+
+    useEffect(() => {
+        async function getProduct() {
+            try {
+                const { data: foundProduct } = await axios.get(`/api/products/${id}`);
+                setProduct(foundProduct);
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        getProduct();
+    }, []);
+
+
+    return (
+        <div>
+            <div id="single_product_container">
+            <h3> Brand: {product.brand} </h3>
+            <h3> Model: {product.model} </h3>
+            <h3> Type: {product.type} </h3>
+            <img src={product.img} />
+            <p> Description: {product.description} </p>
+            <h3> In Stock: {product.quantity} </h3>
+            <h3> Price: {product.price} </h3>
+            </div>
+        </div>
+    );
+}
+
+
+
+export default SingleProduct;
