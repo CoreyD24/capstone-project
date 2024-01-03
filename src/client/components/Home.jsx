915bac498ded4bb4b8e228a1d73c41509904b685
Jaylen <jmowens90@gmail.com>
2024-01-03 16:35:52 -0500

    completed home page to show the images from the database

--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -10,7 +10,8 @@ const Home = () => {
     useEffect(() => {
         async function getProduct() {
           try {
-            const { data: foundProducts } = await axios.get(`/api/products/${id}`);
+            const { data: foundProducts } = await axios.get(`/api/products`);
+            console.log(foundProducts)
             setProducts(foundProducts);
           } catch (error) {
             console.error(error);
@@ -20,18 +21,36 @@ const Home = () => {
       }, []);
 
     return (
-        <section id="homeContainer">
-            <section id="banner">
-            <div id="homePagePictureContainer">
-                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.Fv0PoQNbD6nlipJKq01HqwHaFj%26pid%3DApi&f=1&ipt=4e5b59df8822282f9fb33ad96d7b9dcefa5c92a02fa13e6a8c36baf2a8d14a3c&ipo=images" alt="Ecommerce" id="homePagePicture" />
-            </div>
-            <div id="homeDescriptionContainer">
-                <h1>Welcome to our Ecommerce Store!</h1>
-                <p>Check out our latest products and deals.</p>
-            </div>
-            </section>
-            <div id="homeProductContainer">
-                <Link to="/products/1">
+      <section id="homeContainer">
+        <section id="banner">
+          <div id="homePagePictureContainer">
+            <img
+              src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.Fv0PoQNbD6nlipJKq01HqwHaFj%26pid%3DApi&f=1&ipt=4e5b59df8822282f9fb33ad96d7b9dcefa5c92a02fa13e6a8c36baf2a8d14a3c&ipo=images"
+              alt="Ecommerce"
+              id="homePagePicture"
+            />
+          </div>
+          <div id="homeDescriptionContainer">
+            <h1>Welcome to our Ecommerce Store!</h1>
+            <p>Check out our latest products and deals.</p>
+          </div>
+        </section>
+        <div id="techPicks">
+            <h1> Todays Tech Picks! </h1>
+        </div>
+        <div id="homeProductContainer">
+          {products.map((product) => {
+            console.log(product.id)
+            if ( product.id == 1 || product.id == 6 || product.id == 8) {
+               return ( <Link to={`/products/${product.id}`} key={product.id}>
+                  <div className="homeProductCard">
+                    <img src={product.img} alt={product.brand} />
+                  </div>
+                </Link>
+          )}
+        })}
+
+          {/* <Link to="/products/1">
                     <div className="homeProductCard">Product 1</div>
                 </Link>
                 <Link to="/products/6">
@@ -39,8 +58,8 @@ const Home = () => {
                 </Link>
                 <Link to="/products/8">
                     <div className="homeProductCard">Product 3</div>
-                </Link>
-            </div>
+                </Link> */}
+        </div>
       </section>
     );
   };
