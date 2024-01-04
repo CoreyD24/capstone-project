5eb065628cea9e83b8bb0c2f65d1efe06af04278
Carlos Silva <silvacj90@gmail.com>
2024-01-03 20:57:49 -0500

    add quantity increment, and decrement functions

--- src/client/components/Cart.jsx
+++ src/client/components/Cart.jsx
@@ -5,7 +5,7 @@ import "../styles/cart.css";
 
 const Cart = () => {
   const [products, setProducts] = useState([]);
-  const [quantity, setQuantity] = useState([]);
+  const [quantity, setQuantity] = useState(1);
 
   useEffect(() => {
     const getCart = async () => {
@@ -15,6 +15,8 @@ const Cart = () => {
             Authorization: "Bearer " + window.localStorage.getItem("TOKEN"),
           },
         });
+        // Here we are setting the state of products to have the product info,
+        // plus the requested amount from the back end
         setProducts(cartProducts.result);
       } catch (error) {
         console.error(error);
@@ -23,31 +25,63 @@ const Cart = () => {
     getCart();
   }, []);
 
+  const incrementQuantity = async (index) => {
+    setProducts((prevProducts) => {
+      const updatedProducts = [...prevProducts];
+      if (
+        updatedProducts[index].requested_quantity <
+        updatedProducts[index].quantity
+      ) {
+        updatedProducts[index].requested_quantity += 1;
+      }
+      console.log(updatedProducts);
+      return updatedProducts;
+    });
+  };
+  const decrementQuantity = async (index) => {
+    setProducts((prevProducts) => {
+      const updatedProducts = [...prevProducts];
+      if (updatedProducts[index].requested_quantity > 1) {
+        updatedProducts[index].requested_quantity -= 1;
+      }
+      return updatedProducts;
+    });
+  };
+
   return (
     <div>
-      {products ? (
+      {products.length > 0 ? (
         <div>
           <h2> Your Cart Items </h2>
           <div id="cart_container">
-            {products.map((product) => {
-              console.log(product);
+            {/* We need the index in this map in order to choose which product quantity we're going to edit */}
+            {products.map((product, index) => {
               return (
-                <div className="cart_product">
-                  <Link
-                    to={`/products/${product.id}`}
-                    style={{ textDecoration: "none" }}
-                  >
+                <div className="cart_product" key={product.id}>
+                  <Link to={`/products/${product.id}`}>
                     <img src={product.img} />
                   </Link>
-                  <div key={product.id}>
+                  <div>
                     <h3> Brand: {product.brand} </h3>
                     <h3> Price: {product.price} </h3>
-                  </div>
-                  <div className="quantity_adjuster">
-                    <button>-</button>
-                    <input type="text" />
-                    <button>+</button>
                     <p>Quantity: {product.requested_quantity}</p>
+
+                    <div className="quantity_adjuster">
+                      <button onClick={() => decrementQuantity(index)}>
+                        -
+                      </button>
+                      <input
+                        type="text"
+                        min="1"
+                        max={product.quantity}
+                        value={product.requested_quantity}
+                        onChange={(e) => setQuantity(e.target.value)}
+                      />
+                      <button onClick={() => incrementQuantity(index)}>
+                        +
+                      </button>
+                      <button>UpdateCart</button>
+                    </div>
                   </div>
                 </div>
               );
@@ -55,7 +89,7 @@ const Cart = () => {
           </div>
         </div>
       ) : (
-        ""
+        <p>Your cart is empty</p>
       )}
     </div>
   );
